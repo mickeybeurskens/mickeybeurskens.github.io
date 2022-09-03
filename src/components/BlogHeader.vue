e36a1abe18d0022c568f5c6f2f322005a23ac93d
mickeybeurskens <mickeybeurskens@gmail.com>
2022-09-03 16:11:45 +0200

    Initial commit

---
+++ src/components/BlogHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-center my-5">
+    <router-link
+      :to="'/'"
+      class="text-reset"
+    >
+      <img
+        alt="Vue logo"
+        src="../assets/logo.svg"
+      >
+    </router-link>
+  </div>
+</template>
