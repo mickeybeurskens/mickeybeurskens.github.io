e1b39ea0a944d9602245b513d7ab7f3733d00ca8
mickeybeurskens <mickeybeurskens@gmail.com>
2022-09-03 23:01:42 +0200

    Add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,18 @@
 <template>
   <div
-    class="text-center mt-5 py-2"
-    :style="`background-color: ${VUE_APP_FOOTER_BG_CSS_COLOR}; color: ${VUE_APP_FOOTER_TEXT_CSS_COLOR};`"
+    class="text-center mt-5 py-2 background-footer"
   >
     <router-link
       :to="'/'"
       class="text-reset"
     >
-      <p class="m-0 p-0">
-        vue3-md-blog v{{ version }}
-      </p>
     </router-link>
-    <p class="m-0 p-0">
-      Created with ❤️ by <a
+    <p class="m-0 p-0 footer-text">
+      Created by <a
         href="https://github.com/yeikiu/"
         target="_blank"
         class="text-reset"
-      >JQ</a> © {{ new Date().toDateString().slice(-4) }}
+      >Forge Fire 🔥 Software Engineering</a> © {{ new Date().toDateString().slice(-4) }}
     </p>
   </div>
 </template>
@@ -24,15 +20,13 @@
 <script language="ts">
 import { defineComponent } from 'vue'
 import { version } from '../../package.json'
+import '../styles/footer.scss'
 
-const { VUE_APP_FOOTER_BG_CSS_COLOR = 'darkgray', VUE_APP_FOOTER_TEXT_CSS_COLOR = 'white' } = {}
 
 export default defineComponent({
   data () {
     return {
       version,
-      VUE_APP_FOOTER_BG_CSS_COLOR,
-      VUE_APP_FOOTER_TEXT_CSS_COLOR
     }
   }
 })
