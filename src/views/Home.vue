f9d45fc8605e6e91fd50531d2dd3aebc0fd8522d
Mickey Beurskens <mickeybeurskens@gmail.com>
2024-04-07 13:06:53 +0200

    Remove old introduction

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,97 +6,74 @@
     <!-- HEADER -->
     <div>
       <div class="container pb-4 px-3 px-3 px-md-4 about">
-        <BlogHeader class="mb-5 pt-5" />
         <div class="about-text">
-          <p>
-            Hello there internet traveller, welcome to my blog! You will find
-            all kinds of content here that I find interesting and/or useful.
-            The writing helps me think through challenging topics. I share this in the hopes that might help you too.
-          </p>
 
-          <p>
-            In my working life I am an AI Engineer at my own company, <a href="https://www.forgefire.dev/">Forge Fire</a>,
-            and like to write on the topic both technically and philosophically.
-            I'm particularly interested in autonomous agents and AI alignment.
-          </p>
-          <p>
-            Outside of that I enjoy philosophy, psychology, finance, and the occasional bit of fiction.
-          </p>
-
-          <p>
-            You can reach me through twitter <a href="https://twitter.com/mickeybeurskens">@mickeybeurskens</a>, or even
-            <a href="https://www.linkedin.com/in/mickey-beurskens/">LinkedIn</a> . I'm always happy to chat.
-          </p>
-
-          <p>Enjoy the read.</p>
-
-          <p class="sign">Mickey Beurskens</p>
         </div>
       </div>
 
       <hr />
-      <div class="posts-container mt-5">
-        <div class="higlight-post px-3 px-md-4 pt-3 pl-md-4">
-          <router-link :to="`/${pageStatus.highlightPost.id}`">
-            <h1>{{ pageStatus.highlightPost.title }}</h1>
+
+      <div class="higlight-post mb-5 px-3 px-md-4 pt-3 pl-md-4">
+        <router-link :to="`/${pageStatus.highlightPost.id}`">
+          <h1>{{ pageStatus.highlightPost.title }}</h1>
+        </router-link>
+        <p class="font-weight-light font-italic m-0 p-0 text-left">
+          {{ pageStatus.highlightPost.date }}
+        </p>
+
+        <div v-if="!section && Array.isArray(pageStatus.highlightPost.section)"
+          class="tag-container-left mt-1 markdown-body">
+          <router-link v-for="(sec, index) in pageStatus.highlightPost.section" :key="index" :to="`/blog/${sec}`"
+            class="text-reset tag-item ml-0 mr-1">
+            <h6 class="m-0 p-0 text-left font-weight-bold">
+              #{{ sec }}
+            </h6>
           </router-link>
-          <p class="font-weight-light font-italic m-0 p-0 text-left">
-            {{ pageStatus.highlightPost.date }}
+        </div>
+
+        <div class="highlight-post-content mt-3">
+          <span class="markdown-body" v-html="highlightPostContent" />
+          <p>...</p>
+          <SubscribeButton class="mt-2" :url="'/' + highlightPostUrl" buttonText="Continue Reading" />
+        </div>
+      </div>
+
+      <hr/>
+
+      <div class="recent-posts mt-4 px-3 px-md-4 pt-3 pl-md-4">
+
+        <div class="posts-title">
+          <h1>Recent Posts</h1>
+        </div>
+        <div v-for="entry in pageStatus.summaryPosts" :key="entry.id" class="markdown-body mt-4">
+          <!-- TITLE -->
+          <router-link :to="`/${entry.id}`" class="text-reset">
+            <h3 class="text-left m-0 p-0">
+              {{ entry.title }}
+            </h3>
+          </router-link>
+
+          <!-- POST DETAILS -->
+          <p class="font-weight-light font-italic m-0 p-0" :class="!section ? 'text-right' : 'mb-3'">
+            {{ entry.date }}
           </p>
 
-          <div v-if="!section && Array.isArray(pageStatus.highlightPost.section)" class="tag-container-left mt-1 markdown-body">
-            <router-link v-for="(sec, index) in pageStatus.highlightPost.section" :key="index" :to="`/blog/${sec}`"
-              class="text-reset tag-item ml-0 mr-1">
-              <h6 class="m-0 p-0 text-left font-weight-bold">
+          <div v-if="!section && Array.isArray(entry.section)" class="tag-container">
+            <router-link v-for="(sec, index) in entry.section" :key="index" :to="`/blog/${sec}`"
+              class="text-reset tag-item">
+              <h6 class="m-0 p-0 text-right font-weight-bold">
                 #{{ sec }}
               </h6>
             </router-link>
           </div>
 
-          <div class="highlight-post-content mt-3">
-            <span class="markdown-body" v-html="highlightPostContent" />
-            <p>...</p>
-            <SubscribeButton class="mt-2" :url="'/' + highlightPostUrl" buttonText="Continue Reading" />
-          </div>
+          <!-- POST INTRO -->
+          <p class="font-weight-light mt-1">
+            {{ entry.description }}
+          </p>
         </div>
-
-        <div class="recent-posts px-3 px-md-4 pt-3 pl-md-4">
-
-          <div class="posts-title">
-            <h1>Recent Posts</h1>
-
-            <p>A selection of recent posts. You can also find older posts on the blog page.</p>
-          </div>
-          <div v-for="entry in pageStatus.summaryPosts" :key="entry.id" class="markdown-body mt-4">
-            <!-- TITLE -->
-            <router-link :to="`/${entry.id}`" class="text-reset">
-              <h3 class="text-left m-0 p-0">
-                {{ entry.title }}
-              </h3>
-            </router-link>
-
-            <!-- POST DETAILS -->
-            <p class="font-weight-light font-italic m-0 p-0" :class="!section ? 'text-right' : 'mb-3'">
-              {{ entry.date }}
-            </p>
-
-            <div v-if="!section && Array.isArray(entry.section)" class="tag-container">
-              <router-link v-for="(sec, index) in entry.section" :key="index" :to="`/blog/${sec}`"
-                class="text-reset tag-item">
-                <h6 class="m-0 p-0 text-right font-weight-bold">
-                  #{{ sec }}
-                </h6>
-              </router-link>
-            </div>
-
-            <!-- POST INTRO -->
-            <p class="font-weight-light mt-1">
-              {{ entry.description }}
-            </p>
-          </div>
-          <div class="posts-footer">
-            <SubscribeButton class="mt-2" :url="'/blog'" buttonText="All Blog Posts" />
-          </div>
+        <div class="posts-footer">
+          <SubscribeButton class="mt-2" :url="'/blog'" buttonText="All Blog Posts" />
         </div>
       </div>
     </div>
@@ -189,14 +166,15 @@ h3 {
   text-transform: capitalize;
 }
 
-.tag-container, .tag-container-left {
+.tag-container,
+.tag-container-left {
   display: flex;
   flex-wrap: wrap;
   justify-content: flex-start;
   margin-top: 4px;
 }
 
-.tag-container{
+.tag-container {
   justify-content: flex-end;
 }
 
@@ -216,7 +194,8 @@ h3 {
 
 .higlight-post {
   flex-grow: 1;
-  max-width: 50%;
+  max-width: $max-reading-content-width;
+  margin: auto;
 }
 
 @media (max-width: 900px) {
@@ -231,5 +210,7 @@ h3 {
 
 .recent-posts {
   flex-grow: 1;
+  max-width: $max-reading-content-width;
+  margin: auto;
 }
 </style>../utils/loadMarkdown
