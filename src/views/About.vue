e0f16dcb85ab32b0237fa311528ca1f5639e597b
mickeybeurskens <mickeybeurskens@gmail.com>
2024-07-24 00:39:08 +0200

    Update about section

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,31 +1,27 @@
 <template>
     <div class="container m-top">
-      <p>Welcome to my website, dear reader.</p>
-      <p>You will find that I address a wide range of topics here, ranging from
-         artificial intelligence to philosophy. If there is a red thread in my writing,
-         then I suppose we will be discovering it together. 
+      <h1>About This Website</h1>
+      <p>Welcome, dear reader.</p>
+      <p>On this website I write about topics that I care about.
+        </p>
+        <p>
+          Born and raised a Dutchman, I work as an <a href="https://forgefire.dev">entrepreneur and artificial intelligence engineer.</a>
+          Usually I spend my time figuring out if AI can solve a certain problems, do research, build prototypes and generally expand my knowledge on the subject.
+        </p>  
+        <p>
+          Given my background in robotics and my work I tend to think a lot about AI. I try to get a grip on the major problems that we might face in the long term, and what we might to
+          mitigate the challenges and risks. 
         </p>
         <p>
           Mostly I try to write content that is accessible to everyone, but once in a while I
           write very technical posts. I will try to make it clear which one is which so you can pick and choose what to read. 
         </p>
         <p>
-          In daily life I work as a <a href="https://forgefire.dev">freelance artificial intelligence engineer.</a>
-          Practically that means I am a crazed man with a hammer running around searching for nails to hit, 
-          although I prefer to think of myself as a craftsman and my work as something that deserves considerable care. 
-          Usually I help clients figure out if AI can solve their problems, do research, build prototypes and contribute features to their products.
-        </p>  
-        <p>
-          If you have already read some of my writing, you will know that I also think a lot about
-          AI risk and safety in particular. I try to get a grip on the major problems that we might face in the long term, and what we might to
-          mitigate the problems. 
-        </p>
-        <p>
           Other times I just write about anything that catches my attention. You might find writing on design,
           video games, music, finance or psychology. 
         </p>
         <p>
-          I hope you will enjoy the journey, and a hearthy thank you for reading my work.
+          I hope you will enjoy the journey. Thank you for reading my work.
         </p>
         <p class="sign"> - Mickey Beurskens </p>
     </div>
